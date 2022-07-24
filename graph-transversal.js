cbe8feb08aba196d147ca7f12918a9de2dfc1579
sbkcoder <simonbknight@gmail.com>
2022-07-24 09:48:55 -0500

    dfs

--- graph-transversal.js
+++ graph-transversal.js
@@ -1,4 +1,5 @@
-const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(" ");
+// DATA
+const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
 
 const routes = [
     ['PHX', 'LAX'],
@@ -11,26 +12,58 @@ const routes = [
     ['MEX', 'LIM'],
     ['MEX', 'EZE'],
     ['LIM', 'BKK'],
+];
 
 
+// The graph
+const adjacencyList = new Map();
 
-
-
-]
-
-const adjeancyList = new Map();
-
-
+// Add node
 function addNode(airport) {
-    adjeancyList.set(airport, []);
+    adjacencyList.set(airport, []);
 }
 
+// Add edge, undirected
 function addEdge(origin, destination) {
-    adjeancyList.get(origin).push(destination);
-    adjeancyList.get(destination).push(origin);
+    adjacencyList.get(origin).push(destination);
+    adjacencyList.get(destination).push(origin);
 }
 
+// Create the Graph
 airports.forEach(addNode);
-routes.forEach(route => addEdge(...route));
+routes.forEach(route => addEdge(...route))
 
-console.log(adjeancyList);
+function bfs(start) {
+
+    const visited = new Set();
+
+    const queue = [start];
+
+
+    while (queue.length > 0) {
+
+        const airport = queue.shift(); // mutates the queue
+
+        const destinations = adjacencyList.get(airport);
+
+
+        for (const destination of destinations) {
+
+            if (destination === 'BKK')  {
+                console.log(`BFS found Bangkok!`)
+            }
+
+            if (!visited.has(destination)) {
+                visited.add(destination);
+                queue.push(destination);
+                console.log(destination);
+            }
+           
+        }
+
+        
+    }
+
+}
+
+bfs('PHX')
